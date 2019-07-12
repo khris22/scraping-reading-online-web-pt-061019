56d36258c46dc7a25decb0d149f7b30948680901
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-12 12:24:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ courses = doc.css(".tout__label heading heading--level-4")
 
 courses.each do |course|
   puts course.text.strip
+end
 
 # doc = Nokogiri::HTML(open("http://flatironschool.com/"))
 # p doc.css(".site-header__hero__headline").text.strip
