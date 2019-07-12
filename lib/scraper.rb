0bad8bbec46f9a18c9201b46e7efa51351642450
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-12 12:25:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
-courses = doc.css(".tout__label heading heading--level-4")
+courses = doc.css(".tout__label.heading.heading--level-4")
 
 courses.each do |course|
   puts course.text.strip
