8393dcdf3b1117a999c7f8a764fa03e4148795e5
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-12 12:23:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
-doc.css(".tout__label heading heading--level-4")
+puts doc.css(".tout__label heading heading--level-4")
 
 # doc = Nokogiri::HTML(open("http://flatironschool.com/"))
 # p doc.css(".site-header__hero__headline").text.strip
